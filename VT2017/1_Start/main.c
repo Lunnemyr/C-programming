bb3c6f0794e30fd449636d1cfdc83febd142cabc
HENRIK LUNNEMYR <henrik.lunnemyr@gmail.com>
2017-01-17 19:15:44 +0100

    Tror jag är färdig med mxdiflg-funktionen. Har kört en del tester och hittat flera buggar

--- VT2017/1_Start/main.c
+++ VT2017/1_Start/main.c
@@ -45,15 +45,20 @@ size_t calcShortestStringInArray (const char ** anArrayPtr, size_t arrayLength)
 int mxdiflg(const char **firstArray, size_t firstArrayLength, const char **secondArray, size_t secondArrayLength)
 {
     int result = 0;
+
+
+
     if (firstArray == NULL || secondArray == NULL)
-    {result = -1;}
+    {
+    result = -1;
+    printf("En av arrayerna var NULL\n");}
     else
     {
-        size_t minl1 = 0;
-        size_t maxl1 = 0;
-        size_t minl2 = 0;
-        size_t maxl2 = 0;
-
+        size_t minl1 = strlen(firstArray[0]);
+        size_t maxl1 = strlen(firstArray[0]);
+        size_t minl2 = strlen(secondArray[0]);
+        size_t maxl2 = strlen(secondArray[0]);
+        printf("mxdiflg startvärden: \nMin1 = %zd\nMax1 = %zd\nMin2 = %zd\nMax2 = %zd\n", minl1, maxl1, minl2, maxl2);
         /* Find shortest string in first array*/
         for (int i=0; i<firstArrayLength;i++)
         {
@@ -89,25 +94,55 @@ int mxdiflg(const char **firstArray, size_t firstArrayLength, const char **secon
             }
         }
 
+        printf("mxdiflg beräknar: \nMin1 = %zd\nMax1 = %zd\nMin2 = %zd\nMax2 = %zd\n", minl1, maxl1, minl2, maxl2);
+        result = abs(maxl1-minl2);
+        if (abs(maxl2-minl1)>abs(maxl1-minl2))
+        {
+            result = abs(maxl2-minl1);
+        }
     }
-
     return result;
-
-
-
-
 }
 
 
 int main()
 {
 
-    const char * array1[] = {"", "Gnu", "Hoppsan", "Fem", "Hoppsan", "Hoppsans", "R"};
+    const char * array1[] = {"rastlos", "Gnu", "Hoppsan", "Fem", "Hoppsan", "Hoppsans", "R"};
     size_t firstArrayLength = sizeof(array1)/sizeof(array1[0]);
     printf("Length of array 1 is: %zd\n", firstArrayLength);
-    const char * array2[] = {""};
+
+    const char * array2[] = {"Kalleballeballe", "hopparesnoppare", "Gnuburgarehoppsan"};
     size_t secondArrayLength = sizeof(array2)/sizeof(array2[0]);
+    printf("Length of array 2 is: %zd\n", secondArrayLength);
+
     const char ** array3 = NULL;
+    size_t thirdArrayLength = sizeof(array3)/sizeof(array3[0]);
+    printf("Length of array 3 is: %zd\n", thirdArrayLength);
+
+    const char ** array4 = NULL;
+    size_t fourthArrayLength = sizeof(array4)/sizeof(array4[0]);
+    printf("Length of array 4is: %zd\n", fourthArrayLength);
+
+    /* TEST 1*/
+    printf("\n\nTest 1\n");
+    int expectedResult = 16;
+    int actualResult = mxdiflg(array1, firstArrayLength, array2, secondArrayLength);
+    printf("Expected Result = %d\nActual Result = %d\n", expectedResult, actualResult);
+
+
+    /* TEST 2*/
+    printf("\n\nTest 2\n");
+    expectedResult = -1;
+    actualResult = mxdiflg(array1, firstArrayLength, array3, thirdArrayLength);
+    printf("Expected Result = %d\nActual Result = %d\n", expectedResult, actualResult);
+
+
+    /* TEST 3*/
+    printf("\n\nTest 3\n");
+    expectedResult = -1;
+    actualResult = mxdiflg(array4, fourthArrayLength, array3, thirdArrayLength);
+    printf("Expected Result = %d\nActual Result = %d\n", expectedResult, actualResult);
 
     /*
     printf("Checking if array 1 is empty, result is: %d\n", checkArrayEmpty(array1));
